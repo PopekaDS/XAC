6200a9fb3cd38d014e98666101b65d452b637c66
PopekaDS <popekads@yandex.ru>
2024-11-26 23:58:28 +0400

    Решена задача Monotonic Array

---
+++ algorithms/Array/monotonic-array/monotonic-array.cpp
@@ -0,0 +1,58 @@
+/*
+896. Monotonic Array
+
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+Example 1:
+Input: nums = [1,2,2,3]
+Output: true
+
+Example 2:
+Input: nums = [6,5,4,4]
+Output: true
+
+Example 3:
+Input: nums = [1,3,2]
+Output: false
+ 
+
+Constraints:
+1 <= nums.length <= 10^5
+-10^5 <= nums[i] <= 10^5
+*/
+
+class Solution {
+private:
+    bool isMonotonicUp(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); ++i) {
+            if(nums[i - 1] > nums[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isMonotonicDown(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); ++i) {
+            if(nums[i - 1] < nums[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+public:
+    bool isMonotonic(vector<int>& nums) {
+        if(isMonotonicUp(nums) || isMonotonicDown(nums)) {
+            return true;
+        } 
+
+        return false;
+    }
+};
+
+// Время: O(n)
+// Память: O(1)
