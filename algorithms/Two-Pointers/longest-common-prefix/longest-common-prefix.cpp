66c7818cf1e375e7473a040cf1172fa4f71a4f4e
PopekaDS <popekads@yandex.ru>
2024-12-09 20:03:59 +0400

    Решена задача Longest Common Prefix

---
+++ algorithms/Two-Pointers/longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,52 @@
+/*
+14. Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+Constraints:
+
+* 1 <= strs.length <= 200
+* 0 <= strs[i].length <= 200
+* strs[i] consists of only lowercase English letters.
+*/
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int _min = strs[0].size();
+    
+        for(auto now: strs) {
+            int nowsize = now.size();
+            _min = min(_min, nowsize);
+        }
+        
+        int i = 0;
+        string result = "";
+        
+        while(i < _min) {
+            char currChar = strs[0][i];
+            for(auto now: strs) {
+                if(now[i] != currChar) {
+                    return result;
+                }
+            }
+            
+            result += currChar;
+            ++i;
+        }
+        
+        return result;
+        
+    }
+};
+// Время: O(S)
+// Память: O(1)
